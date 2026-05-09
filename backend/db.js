59f45fff27e7e206b2d4cc974a461f912873451a
Julia Delis <juliadelis2@hotmail.com>
2026-05-09 13:33:55 -0300

    feat: test

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connectionString = process.env.DB_URL;
-const db = postgres(connectionString);
+
+if (!connectionString) {
+  throw new Error("DB_URL is not defined");
+}
+
+const db = postgres(connectionString, {
+  ssl: "require",
+  max: 1,
+  idle_timeout: 20,
+  connect_timeout: 10,
+});
 
 module.exports = { db };
